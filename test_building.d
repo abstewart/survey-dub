872e132cb5ccfba128498626e7e8f8a058ee35aa
abstewart <26328673+abstewart@users.noreply.github.com>
2023-09-01 14:54:49 +0100

    Update test_building with functional flow for 1 package

--- test_building.d
+++ test_building.d
@@ -5,12 +5,67 @@ import std.file;
 import std.algorithm;
 
 
+
 void main () {
 
-  //fetch the package
-  auto fetch_cmd = "dub fetch " ~ "unitthreaded";
-  auto output = executeShell (fetch_cmd);
-  writeln (output.output);
+  survey_package ("libasync");
+  remove_package ("libasync");
+}
 
-  
+
+bool survey_package (string pkgname){
+  string pkgdir;
+  return fetch_package (pkgname) &&
+    build_package(pkgname, pkgdir) &&
+    test_package (pkgname, pkgdir);
+
+}
+
+
+bool fetch_package (string pkgname){
+  //fetch the package
+  auto fetch_cmd = "dub fetch " ~ pkgname;
+  auto output = executeShell (fetch_cmd);
+
+  debug writeln(output.status);
+  debug writeln(output.output);
+
+  //check for success
+  return output.status == 0;
+}
+
+
+bool build_package (string pkgname, ref string pkgdir){
+  //build the package
+  auto build_cmd = "dub build " ~ pkgname;
+  auto output = executeShell (build_cmd);
+
+  debug writeln(output.status);
+  debug writeln(output.output);
+
+  //get the directory the package was built from and assign to ref input
+  pkgdir = output.output.splitLines()[0].split(" ")[$-1];
+
+  //check for success
+  return output.status == 0;
+}
+
+
+bool test_package (string pkgname, string pkgdir){
+  //navigate to the dub installation & run the test command
+  auto test_cmd = "dub test";
+  //auto output = executeShell (test_cmd, workdir=pkgdir);
+  auto output = executeShell(test_cmd, null, Config(Config.Flags.none, null), 18446744073709551615LU, pkgdir);
+
+  debug writeln(output.status);
+  debug writeln(output.output);
+
+  return output.status == 0;
+}
+
+void remove_package (string pkgname) {
+  auto remove_cmd = "dub remove " ~ pkgname;
+  auto output = executeShell(remove_cmd);
+  debug writeln(output.status);
+  debug writeln(output.output);
 }
